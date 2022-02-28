093a0df65c824e5a9e2043d25936907efcf88065
sergeishel <81753078+sergeishel@users.noreply.github.com>
2022-02-28 11:28:45 +0300

    Add files via upload

---
+++ shel/Meditation/SingInViewController.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class SingInViewController: UIViewController {
+
+    @IBOutlet weak var inputPassword: UITextField!
+    @IBAction func signInAction(_ sender: UIButton) {
+    }
+}
+
